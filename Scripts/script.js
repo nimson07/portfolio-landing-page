48f2f29089d140837c0232d8dce441b1daa4c3ae
nimson07 <nimdiej055@gmail.com>
2025-02-27 11:27:02 +0000

    fix6

--- Scripts/script.js
+++ Scripts/script.js
@@ -17,3 +17,30 @@ function resetTypingAnimation() {
 }
 
 setInterval(resetTypingAnimation, 6500); // Reset every 6 seconds (4s typing + 2s delay)
+
+
+let currentIndex = 0;
+        const slides = document.querySelectorAll('.slide');
+        const dots = document.querySelectorAll('.dot');
+
+        function showSlide(index) {
+            slides.forEach((slide, i) => {
+                slide.classList.remove('active');
+                dots[i].classList.remove('active-dot');
+                if (i === index) {
+                    slide.classList.add('active');
+                    dots[i].classList.add('active-dot');
+                }
+            });
+        }
+
+        function currentSlide(index) {
+            currentIndex = index - 1; // Convert to zero-based index
+            showSlide(currentIndex);
+        }
+
+        // Auto slide every 5 seconds
+        setInterval(() => {
+            currentIndex = (currentIndex + 1) % slides.length;
+            showSlide(currentIndex);
+        }, 5000)
